e2d23b37c48790547de172576ac58e957b438d78
MoisesLlamasGarcia <moisesllamasgarcia@gmail.com>
2015-11-13 13:47:45 +0100

    Modificacion del programa contador para que cuente al reves
    Se ha modificado el programa contador, escrito en Java para que cuente
    desde los valores especificados, en este caso desde el 10 al 1

--- cuenta.java
+++ cuenta.java
@@ -1,6 +1,7 @@
 // Esto es un programa que cuenta desde el 1 al 10
 int i;
-for (i=0;i<=10;i++){
-i=i++;
+// se modifica el for para que cuente desde el 10 al 1
+for (i=10;i<0;i--){
+i=i--;
 System.out.println("Voy contando por el numero:" +i);
 }
