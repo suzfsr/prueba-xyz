ed7c1dff7b10db2c97a2ed32fbf4c745cca34b51
MoisesLlamasGarcia <moisesllamasgarcia@gmail.com>
2015-11-13 13:52:56 +0100

    Modificamos el programa contador
    Se modifica el programa contador escrito en Python y lo guardamos en la nueva ramma "next"

--- cuenta.py
+++ cuenta.py
@@ -1,6 +1,6 @@
 # Programa contador en Python
 print("Comienzo")
-# Añadimos el rango que queremos que cuente
+# Modificamos y Añadimos el rango que queremos que cuente
 for i in [1,10]:
     print("Hola. Ahora i vale", i)
 print("Final")
