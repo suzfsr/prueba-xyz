2b88fb57c110dfa7deaea18c1b4d7acbe28dd836
MoisesLlamasGarcia <moisesllamasgarcia@gmail.com>
2015-11-13 13:55:14 +0100

    Modificacion del contador en Python
    Se modifica el programa contador de nuevo

--- cuenta.py
+++ cuenta.py
@@ -1,6 +1,6 @@
 # Programa contador en Python
 print("Comienzo")
-# Añadimos el rango que queremos que cuente
+# Añadimos el rango que queremos que cuente en la rama master
 for i in [1,10]:
     print("Hola. Ahora i vale", i)
 print("Final")
